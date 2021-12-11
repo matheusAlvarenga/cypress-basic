94c339c1e487d68b15ec2ea10d4fedff00ad883f
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2021-12-11 16:10:42 -0300

    feat: create tickets test file and visit tickets page

---
+++ cypress/integration/tickets.spec.js
@@ -0,0 +1,7 @@
+describe('Tickets', () => {
+  beforeEach(() => cy.visit('https://ticket-box.s3.eu-central-1.amazonaws.com/index.html'))
+
+  it('should have "ticketbox" headers heading', () => {
+    
+  });
+})
